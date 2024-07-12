8d38decddeca06893b54e50c0e9019af568675c5
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-07-12 13:20:10 +0300

    Validated values

--- app/main.py
+++ app/main.py
@@ -1,24 +1,41 @@
 class Car:
     def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
             -> None:
-        if 1 <= comfort_class <= 7:
-            self.comfort_class = comfort_class
-        if 1 <= clean_mark <= 10:
-            self.clean_mark = clean_mark
+        if not (1 <= comfort_class <= 7):
+            raise ValueError(
+                "The value of `comfort_class` must be in the range from 1 to 7"
+            )
+        self.comfort_class = comfort_class
+        if not (1 <= clean_mark <= 10):
+            raise ValueError(
+                "The value of `clean_mark` must be in the range from 1 to 10"
+            )
+        self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
-        if 1.0 <= distance_from_city_center <= 10.0:
-            self.distance_from_city_center = distance_from_city_center
-        if 1 <= clean_power <= 10:
-            self.clean_power = clean_power
-        if 1.0 <= average_rating <= 5.0:
-            self.average_rating = average_rating
-        if count_of_ratings > 0:
-            self.count_of_ratings = count_of_ratings
+        if not (1.0 <= distance_from_city_center <= 10.0):
+            raise ValueError(
+                "The value of `distance_from_city_center` "
+                "must be in the range from 1.0 to 10.0"
+            )
+        self.distance_from_city_center = distance_from_city_center
+        if not (1 <= clean_power <= 10):
+            raise ValueError(
+                "The value of `clean_power` must be in the range from "
+                "1 to 10"
+            )
+        self.clean_power = clean_power
+        if not (1.0 <= average_rating <= 5.0):
+            raise ValueError(
+                "The value of `average_rating` must be in the range from "
+                "1.0 to 10.0"
+            )
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class
