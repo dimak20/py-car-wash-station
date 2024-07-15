95629d05984a60cf3393f87b77da0011bc8faee7
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-07-15 11:07:35 +0300

    Validated values using unified method

--- app/main.py
+++ app/main.py
@@ -1,40 +1,19 @@
 class Car:
     def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
             -> None:
-        if not (1 <= comfort_class <= 7):
-            raise ValueError(
-                "The value of `comfort_class` must be in the range from 1 to 7"
-            )
-        self.comfort_class = comfort_class
-        if not (1 <= clean_mark <= 10):
-            raise ValueError(
-                "The value of `clean_mark` must be in the range from 1 to 10"
-            )
-        self.clean_mark = clean_mark
+        self.comfort_class = validate(comfort_class, 1, 7)
+        self.clean_mark = validate(clean_mark, 1, 10)
         self.brand = brand
 
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
-        if not (1.0 <= distance_from_city_center <= 10.0):
-            raise ValueError(
-                "The value of `distance_from_city_center` "
-                "must be in the range from 1.0 to 10.0"
-            )
-        self.distance_from_city_center = distance_from_city_center
-        if not (1 <= clean_power <= 10):
-            raise ValueError(
-                "The value of `clean_power` must be in the range from "
-                "1 to 10"
-            )
-        self.clean_power = clean_power
-        if not (1.0 <= average_rating <= 5.0):
-            raise ValueError(
-                "The value of `average_rating` must be in the range from "
-                "1.0 to 5.0"
-            )
-        self.average_rating = average_rating
+        self.distance_from_city_center = validate(
+            distance_from_city_center, 1.0, 10.0
+        )
+        self.clean_power = validate(clean_power, 1, 10)
+        self.average_rating = round(validate(average_rating, 1.0, 5.0), 1)
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
@@ -56,6 +35,15 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, mark: int) -> None:
-        total = self.count_of_ratings * self.average_rating
+        total = self.count_of_ratings * self.average_rating + mark
         self.count_of_ratings += 1
-        self.average_rating = round((total + mark) / self.count_of_ratings, 1)
+        self.average_rating = round(total / self.count_of_ratings, 1)
+
+
+def validate(number: float, lower: float, upper: float) -> float:
+    if not (lower <= number <= upper):
+        raise ValueError(
+            f"The value of {number} must be in the range from "
+            f"{lower} to {upper}"
+        )
+    return number
